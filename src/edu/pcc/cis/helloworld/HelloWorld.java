ceb40b23c7118afff6f939a5a3f11e562764e9ff
Long Vinh Nguyen <andy.nguyen4@pcc.edu>
2018-02-06 17:28:57 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,12 @@
 package edu.pcc.cis.helloworld;
-
+import java.util.Date;
+/*
+    Create by Long Vinh Nguyen on 2.6.2018
+ */
 public class HelloWorld {
     public static  void main (String[] args) {
         System.out.println("Hello World");
+
+        System.out.println(new Date());
     }
 }
